1fa49083218fcdfdb807e49110a9930c9feeabd1
Wadson.matos <wadson.matos@parcelenahora.com.br>
2025-01-03 18:01:39 -0300

    Inserindo loader

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import CaixaDialogo from './components/CaixaDialogo';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Spinner } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
 import { FaExclamationTriangle, FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalPropos, setModalProps] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const abrirModal = () => {    
-    setModalProps({
-        titulo : "Sucesso",
-        conteudo:"A operação foi concluída com sucesso !",
-        icone : <FaCheckCircle  style={{color:'green'}} />
-    });
+  const abrirModalComLoader = async (titulo, conteudo, icone) => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setLoading(false);
+    setModalProps({titulo, conteudo, icone});
     setShowModal(true);
   };
 
@@ -32,18 +32,41 @@ const App = () => {
     <div>
       <Container>
           <h1>Exemplo de Modal com React-Bootstrap</h1>
-          <Row>
-            <Col>
-              <Button variant="primary" onClick={abrirModal}>
+          {loading &&(
+            <div style={{textAlign:'center', margin:'20px 0'}}>
+              <Spinner animation='border' role='status'>
+                  <span className='visually-hidden'>loading ...</span>
+              </Spinner>
+              <p>Carregando ...</p>
+            </div>
+          )}
+
+          {!loading && (
+              <Row>
+              <Col>
+                <Button variant="primary" onClick={() =>abrirModalComLoader(
+                  'Sucesso',
+                  'A operação foi concluída com sucesso !',
+                  <FaCheckCircle style={{ color: 'green' }} />
+                  )}>
+                  Abrir Modal
+                </Button>
+              </Col>
+              <Col>
+              <Button  variant="warning"
+          onClick={() =>
+            abrirModalComLoader(
+              'Aviso',
+              'Essa ação pode ter consequências.',
+              <FaExclamationTriangle style={{ color: 'orange' }} />
+            )
+          }
+        >
                 Abrir Modal
               </Button>
-            </Col>
-            <Col>
-            <Button variant="danger" onClick={abrirModal}>
-              Abrir Modal
-            </Button>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          )}          
       </Container>
 
       {/* Modal CaixaDialogo */}
